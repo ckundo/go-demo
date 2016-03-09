c4caf449466b5b547cb26e9eb4bab06c47fc9a98
Cameron Cundiff <cameron@thoughtbot.com>
2016-03-09 18:48:21 -0500

    WIP

--- integration_test.go
+++ integration_test.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestRequests(t *testing.T) {
+	// fakeServer := httptest.NewServer(http.HandlerFunc(func(...) {
+	// 	w.Write([]byte{})
+	// }))
+
+	// fakeServer.URL
+
 	router := newRouter()
 	server := httptest.NewServer(router)
 	defer server.Close()
@@ -18,5 +25,31 @@ func TestRequests(t *testing.T) {
 
 	if response.StatusCode != http.StatusOK {
 		t.Errorf("Expected 200 response, got %d", response.StatusCode)
+	}
+
+	response, err = http.Get(server.URL + "/nyc")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		t.Errorf("Expected 200 response, got %d", response.StatusCode)
+	}
+
+	var conditions map[string]float64
+
+	dec := json.NewDecoder(response.Body)
+	if err := dec.Decode(&conditions); err != nil {
+		t.Errorf("Expected json with temperature values, got %s", err)
+	}
+
+	feelsLike, ok := conditions["feels_like"]
+
+	if !ok {
+		t.Errorf("Expected feels_like key")
+	}
+
+	if feelsLike == 0 {
+		t.Errorf("Expected feels_like value != 0, got %f", feelsLike)
 	}
 }
--- main.go
+++ main.go
@@ -1,18 +1,56 @@
 package main
 
 import (
-	"github.com/julienschmidt/httprouter"
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
+
+type WeatherResponse struct {
+	CurrentObservation struct {
+		FeelsLike string `json:"feelslike_f"`
+	} `json:"current_observation"`
+}
 
 func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func newRouter() *httprouter.Router {
+func Conditions(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+
+	apiEndpoint := "http://api.wunderground.com/api/<key>/conditions/q/CA/San_Francisco.json"
+	response, err := http.Get(apiEndpoint)
+	defer response.Body.Close()
+	if err != nil {
+		log.Printf("Failed to get conditions: %s\n", err)
+		return
+	}
+
+	var data WeatherResponse
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		panic(err)
+	}
+
+	feelsLike, err := strconv.ParseFloat(data.CurrentObservation.FeelsLike, 64)
+	if err != nil {
+		log.Printf("Failed to convert json from %s\n", err)
+	}
+
+	conditions := map[string]float64{
+		"feels_like": feelsLike,
+	}
+
+	json.NewEncoder(w).Encode(conditions)
+}
+
+func newRouter() http.Handler {
 	router := httprouter.New()
 	router.GET("/", Home)
+	router.GET("/nyc", Conditions)
 
 	return router
 }
